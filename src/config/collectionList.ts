4a79de4a9ace586a17d259552f225c146c44b078
Tristan <delmastristan64@gmail.com>
2024-11-19 16:43:15 +0100

    add classes route

---
+++ src/config/collectionList.ts
@@ -0,0 +1,9 @@
+export enum collectionList {
+  TEST = 'test',
+  USERS = 'users',
+  CLASSES = 'classes',
+  SUBCLASSES = 'subclasses',
+  SPELLS = 'spells',
+  GEARS = 'gears',
+  CHARACTERS = 'characters',
+}
